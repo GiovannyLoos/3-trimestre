1f654d50ae97e67b0ea31bf491166e88a890fa91
GiovannyLoos <164014585+GiovannyLoos@users.noreply.github.com>
2024-10-08 10:48:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-criarCartão(categoria, pergunta, resposta) {
-
-}
-function criarCartão(categoria, pergunta, resposta) {
-    let cotainer = document.getElementById('container')
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+
+    let respostaEstaVisivel = false
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+    container.appendChild(cartao)
 }
-cartao.innerHTML =
-`<div class="cartao_conteudo">
-         <h3>programação<h3>
-         <div class="cartao_conteudo_pergunta">
-            <p>oque é JavaScript?<p>
-        <div>
-        <div class="cartao_conteudo_pergunta">
-           <p> O JavaScript é uma linguagem de programação</p>
-           </div>
-           </div>`
-           Container.appenChild(cartao)
